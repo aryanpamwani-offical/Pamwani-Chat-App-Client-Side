b20e6c75bcdc90bf94f8685444bb10f79a273a31
aryanpamwani-offical <113785853+aryanpamwani-offical@users.noreply.github.com>
2024-05-11 11:40:27 +0530

    Link Connected

--- src/Components/Herosection.js
+++ src/Components/Herosection.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import {NavLink, useNavigate} from 'react-router-dom'
 const Herosection = () => {
   return (
     <>
@@ -8,11 +8,11 @@ const Herosection = () => {
           <div className="col-6 d-flex flex-column my-auto">
             
 
-<h1 className='p-3'>Simplify Your Digital Life with <span className='text-primary'>Self Connect</span></h1>
+<h1 className='p-3'>Simplify Your Digital Life with <span className='text-primary'>Self Connect Chat App</span></h1>
 <div className='px-3'>
 
-  <button className='rounded-4 bg-primary text-white hero-btn my-2 w-25 w-md-50 w-sm-75 w border-0'>Get Started</button>
-</div>
+<NavLink to={'/signup'}><button className='rounded-4 bg-primary text-white hero-btn my-2 w-25 w-md-50 w-sm-75 w border-0'>Get Started</button>
+</NavLink></div>
           </div>
           <div className="col-6   rounded mt-5">
             <img src="/hero.jpg" className='rounded-5 hero mt-5' alt="" />
